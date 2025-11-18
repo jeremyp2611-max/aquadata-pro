9d03ff4fd2d21840a4125d09f45f1db25d180a58
jeremyp2611-max <jeremyp2611@gmail.com>
2025-11-17 21:36:11 -0500

    Update aquadata_app.py

--- aquadata_app.py
+++ aquadata_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
 from datetime import datetime, timedelta
 import requests
 
